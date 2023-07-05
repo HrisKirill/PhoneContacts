a621df36dcf155c5dd76c33d507ffbfee79756f2
HrisKirill <hristoforow.kirill@gmail.com>
2023-07-05 16:06:37 +0300

    fix problem with onetomany

--- src/main/java/com/example/phonecontacts/dao/interfaces/IContactEmailDao.java
+++
@@ -1,8 +0,0 @@
-/*
-package com.example.phonecontacts.dao.interfaces;
-
-import com.example.phonecontacts.entities.ContactEmail;
-
-public interface IContactEmailDao extends Dao<ContactEmail> {
-}
-*/
--- src/main/java/com/example/phonecontacts/dao/interfaces/IContactPhoneNumberDao.java
+++
@@ -1,6 +0,0 @@
-/*package com.example.phonecontacts.dao.interfaces;
-
-import com.example.phonecontacts.entities.ContactPhoneNumber;
-
-public interface IContactPhoneNumberDao extends Dao<ContactPhoneNumber> {
-}*/
