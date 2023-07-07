51c1335cac37ea98561a4d4f61dfa86b4e3804e4
HrisKirill <hristoforow.kirill@gmail.com>
2023-07-07 13:05:51 +0300

    added @NoArgsConstructor to dtos

--- src/main/java/com/example/phonecontacts/dto/LoginDto.java
+++ src/main/java/com/example/phonecontacts/dto/LoginDto.java
@@ -2,9 +2,11 @@ package com.example.phonecontacts.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginDto {
 
     private String login;
